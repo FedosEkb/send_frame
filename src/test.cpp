e469403e3acbfc9ca4f1fd45bc41162dbfae1478
admin <admin332@astra>
2024-06-10 16:41:15 +0500

    Add some comments.

--- src/test.cpp
+++ src/test.cpp
@@ -3,28 +3,29 @@
 
 typedef unsigned int super_uint32_t;
 
-inline uint64_t htonMAC(uint64_t x) { return (((uint64_t)htonl(x)) << 16) + htons((x) >> 32); } __attribute__ ((__const__)) //!  flips the bytes end to end. If you apply it twice, it restores the value to its original state.
-inline uint64_t ntohMAC(uint64_t x) { return (((uint64_t)htonl(x)) << 16) + htons((x) >> 32); } __attribute__ ((__const__)) //!  So the same function can be used for ntohll
-
-
 int main()
 {
 #ifdef ETH_TEST
 
-    RawEthernet ethenet("lo");
+    RawEthernet ethenet("eth0");
     uint8_t buff[0x600];
-    for (size_t i = 0; i < 255; i++)
+    for (size_t i = 0; i < 0x600; i++)
     {
         buff[i] = i;
     }
 
     std::cout << "\nSend frame:\n";
 
-    ethenet.send_ethernet_frame(0x0,buff,255,0xFFF0);
+    
 
-    std::cout << "\nReceive frame:\n";
-    size_t taked_len = ethenet.receive_ethernet_frame(buff,0x600);
-    print_ethernet_header(reinterpret_cast<ethhdr *>(buff));
+    for (size_t i = 0; i < 64; i++)
+    {
+        ethenet.send_ethernet_frame(0x120000000000, buff, 48 + i*8, 0xFF00 + i);
+    }
+
+    // std::cout << "\nReceive frame:\n";
+    // size_t taked_len = ethenet.receive_ethernet_frame(buff,0x600);
+    // print_ethernet_header(reinterpret_cast<ethhdr *>(buff));
 
 #endif /* ETH_TEST */
 
